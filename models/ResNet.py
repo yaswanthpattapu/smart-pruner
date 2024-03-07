908f5d1f603c93644e4a528afe0315109e4f1054
Shyam Sundar <shyamanve@gmail.com>
2024-03-07 22:55:15 +0530

    "Adding Models"

--- models/ResNet.py
+++ models/ResNet.py
@@ -139,13 +139,13 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
 
-# def ResNet50(img_channel=3, num_classes=1000):
-#     return ResNet(block, [3, 4, 6, 3], img_channel, num_classes)
+def ResNet50(img_channel=3, num_classes=1000):
+    return ResNet(block, [3, 4, 6, 3], img_channel, num_classes)
 
 
-# def ResNet101(img_channel=3, num_classes=1000):
-#     return ResNet(block, [3, 4, 23, 3], img_channel, num_classes)
+def ResNet101(img_channel=3, num_classes=1000):
+    return ResNet(block, [3, 4, 23, 3], img_channel, num_classes)
 
 
-# def ResNet152(img_channel=3, num_classes=1000):
-#     return ResNet(block, [3, 8, 36, 3], img_channel, num_classes)
+def ResNet152(img_channel=3, num_classes=1000):
+    return ResNet(block, [3, 8, 36, 3], img_channel, num_classes)
